7fbb8e48acbd297dcca2488abc6af50548757622
TheQueenIsDead <30706552+TheQueenIsDead@users.noreply.github.com>
2022-12-21 11:52:40 +1300

    chore: add more targets and poll to the bootstrap method

--- backend/api/bootstrap.go
+++ backend/api/bootstrap.go
@@ -3,30 +3,68 @@ package api
 import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/gommon/log"
+	"gorm.io/gorm"
 	"net/http"
 	"pingo/models"
+	"time"
 )
 
 func (a *Application) createBootstrap(c echo.Context) error {
 
-	target := &models.Target{Source: "https://google.com", Frequency: 1, Unit: "SECOND"}
-	err := a.db.Create(target).Error
+	// Create targets
+	target := []*models.Target{
+		{Source: "https://google.com", Frequency: 1, Unit: "SECOND"},
+		{Source: "https://github.com", Frequency: 5, Unit: "SECOND"},
+		{Source: "https://example.com", Frequency: 3, Unit: "MINUTE"},
+	}
+	err := a.db.Create(&target).Error
 	if err != nil {
 		log.Error(err)
-		return c.JSON(http.StatusBadRequest, err)
+		return c.JSON(http.StatusInternalServerError, err)
 	}
 
-	return c.JSON(http.StatusOK, target)
+	// Create poll
+	poll := models.Poll{
+		Targets: []models.Target{
+			{Model: gorm.Model{ID: 0}},
+			{Model: gorm.Model{ID: 2}},
+			{Model: gorm.Model{ID: 3}},
+		},
+		DurationSeconds: 60,
+		StartedAt:       time.Time{},
+	}
+	err = a.db.Create(&poll).Error
+	if err != nil {
+		log.Error(err)
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+
+	return c.JSON(http.StatusOK, HttpEmptyJSONResponse)
 }
 
 func (a *Application) deleteBootstrap(c echo.Context) error {
 
-	// Remove all targets
-	err := a.db.Unscoped().Where("1 = 1").Delete(&models.Target{}).Error
+	// Remove all models
+	var ml []interface{}
+	ml = append(ml, &models.Poll{})
+	ml = append(ml, &models.Target{})
 
+	for _, m := range ml {
+		err := a.db.Unscoped().Where("1 = 1").Delete(m).Error
+		if err != nil {
+			log.Error(err)
+			return c.JSON(http.StatusInternalServerError, err)
+		}
+	}
+
+	// Remove all relationships
+	// TODO: Not working, investigate
+	//goland:noinspection SqlWithoutWhere
+	err := a.db.Raw("DELETE FROM poll_targets;").Error
 	if err != nil {
 		log.Error(err)
 		return c.JSON(http.StatusInternalServerError, err)
+
 	}
 
 	return c.JSON(http.StatusOK, HttpEmptyJSONResponse)
